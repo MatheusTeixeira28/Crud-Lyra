9c2c646134a0a6aae89e10ad8646cd4d2603ff4a
Matheus Pinto Teixeira <matheusteixeira29@hotmail.com>
2021-07-13 22:40:47 -0300

    first commit

---
+++ projeto.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const database = require('./db')
+
+const Users = database.define('user', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER(.100)
+      },
+
+      fullname: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      email:{
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+
+      phone:{ 
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      UF:{
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+
+    
+})
+
+module.exports = Users;
